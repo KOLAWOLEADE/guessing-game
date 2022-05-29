fa1aa5e0b5df80e3ac5bcdb557b02b6421f2c22c
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-29 12:54:15 +0100

    add guessNumber

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// GEUSSING GAME
+
+const num = 20;
+
+ guessNumber = parseInt(prompt( "input your geuss number" ))
+
+//  function GEUSSING GAME
+
+    if (guessNumber===num){
+        alert("Congratulation, you just won 10 million");
+        console.log("Congratulation, you just won 10 million");
+        document.write("Congratulation, you just won 10 million");
+
+    }
+
+    else if (guessNumber < num){
+        alert("Try again: The number is greater than your guess number");
+        console.log("Try again: The number is greater than your guess number");
+        document.write("Try again: The number is greater than your guess number");
+    }
+
+    else if  (guessNumber > num){
+        alert("Try again: The number is lesser than your guess number");
+        console.log("Try again: The number is lesser than your guess number");
+        document.write("Try again: The number is lesser than your guess number");
+
+    }
+
+    else {
+        alert("Your number is incorrect");
+        console.log("Your number is incorrect");
+        document.write("Your number is incorrect");
+    }
